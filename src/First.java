07c2bc987f3ede1eaf7bd560e2ff53f8a83bdc96
abhay.sharma <abhay.sharma@git.u2opia.local:/repos/testrepo>
2015-04-21 13:43:06 +0530

    my firstProgram

---
+++ src/First.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class First {
+   
+	double width;
+	double height;
+	double depth;
+	
+	First (double w,double h,double d){
+		//System.out.println("Constructing First class");
+		this.width =w;
+		this.height =h;
+		this.depth =d;
+	}
+	
+    double volume () {
+	return width * height * depth;
+}
+  }
+
+
+   
+
+  
---
+++ src/FirstDemo.java
@@ -0,0 +1,16 @@
+   public class FirstDemo {
+    	
+    	public static void main (String args[]){
+    		First myobj1 =new First(20,13,20);
+    		First myobj2 =new First(3,5,9);
+    		
+    		double vol;
+    		vol = myobj1.volume();
+    		System.out.println("Volume is" + vol);
+    		vol = myobj2.volume();
+    		System.out.println("Volume is " + vol);
+    		
+    		
+    	}
+    }
+ 
